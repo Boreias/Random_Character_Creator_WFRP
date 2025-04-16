99b7dd919bc59275d5bd0aa6d586341768851aaa
Boreias <danielroberto95@hotmail.com>
2025-04-16 19:21:03 -0300

    Criação de personagem e populando atributos

--- RandomCharacterCreator_WFRP/src/character.rs
+++ RandomCharacterCreator_WFRP/src/character.rs
@@ -3,6 +3,29 @@ use postgres::{Client, NoTls};
 use rand::Rng;
 
 
+pub struct Species {
+    id: i32,
+    name: String,
+}
+
+impl Species {
+    pub fn new(id: i32, name: String) -> Self {
+        Species { id, name }
+    }
+}
+
+pub struct Career {
+    id: i32,
+    name: String,
+}
+
+impl Career {
+    pub fn new(id: i32, name: String) -> Self {
+        Career { id, name }
+    }
+}
+
+
 pub struct Character {
     species: String,
     career: String,
@@ -14,20 +37,37 @@ pub struct Character {
 }
 
 impl Character {
-    pub fn new(species: String, career: String, career_level: u8) -> Self {
+    pub fn new(species: Species, career: Career, career_level: u8) -> Self {
+        let mut client = Client::connect("host=localhost user=postgres dbname=wfrp", NoTls).unwrap();
+
         let mut attributes = HashMap::new();
-        let mut skills = HashMap::new();
+        let mut skills: HashMap<String, u8> = HashMap::new();
         let mut talents = HashMap::new();
         let trappings = Vec::new();
 
+        let species_id = species.id;
+
+        for row in client.query(
+            "SELECT Attributes.Name as attribute, SpeciesAttributes.AdditionalAdjustment as value
+                    FROM SpeciesAttributes
+                    INNER JOIN Attributes ON SpeciesAttributes.AttributesID=Attributes.ID
+                    WHERE SpeciesAttributes.SpeciesID = $1",
+                    &[&species_id]).unwrap() {
+            let attribute_name: String = row.get("attribute");
+            let attribute_value: i32 = row.get("value");
+            attributes.insert(
+                attribute_name,
+                rand::thread_rng().gen_range(1, 11) + rand::thread_rng().gen_range(1, 11) + attribute_value as u8);
+        }
+
         Character {
-            species,
-            career,
-            career_level,
-            attributes,
-            skills,
-            talents,
-            trappings,
+            species: species.name.to_string(),
+            career: career.name.to_string(),
+            career_level: career_level,
+            attributes: attributes,
+            skills: skills,
+            talents: talents,
+            trappings: trappings,
         }
     }
 
--- RandomCharacterCreator_WFRP/src/main.rs
+++ RandomCharacterCreator_WFRP/src/main.rs
@@ -1,8 +1,28 @@
 mod character;
-use character::Character;
+use character::{Character, Species, Career};
 use postgres::{Client, NoTls};
+use rand::Rng;
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut client = Client::connect("host=localhost user=postgres dbname=wfrp", NoTls).unwrap();
+
+    let species_roll = rand::thread_rng().gen_range(1, 101);
+
+    let species_db = client.query_one(
+        "SELECT ID, Name FROM Species WHERE $1 BETWEEN InitialPercentage AND MaxPercentage",
+        &[&&species_roll]).unwrap();
+
+    let species = Species::new(species_db.get(0), species_db.get(1));
+
+
+    let career_roll = rand::thread_rng().gen_range(1, 101);
+
+    let career_db = client.query_one(
+        "SELECT ID, Name FROM Career WHERE $1 BETWEEN InitialPercentage AND MaxPercentage",
+        &[&&career_roll]).unwrap();
+    
+    let career = Career::new(career_db.get(0), career_db.get(1));
+
+    let character = Character::new(species, career, 1);
 }
