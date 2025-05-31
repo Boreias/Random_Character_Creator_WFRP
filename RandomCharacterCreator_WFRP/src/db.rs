3b1966c0daa372d37aa9875f0730ca590d9d3f75
Boreias <danielroberto95@hotmail.com>
2025-05-31 14:44:02 -0300

    Utilização de variáveis de ambiente

--- RandomCharacterCreator_WFRP/src/db.rs
+++ RandomCharacterCreator_WFRP/src/db.rs
@@ -1,4 +1,5 @@
 use postgres::{Client, NoTls, Row, Error};
+use std::env;
 
 pub struct Db {
     client: Client,
@@ -7,7 +8,13 @@ pub struct Db {
 impl Db {
     /// Creates a new database connection
     pub fn connect() -> Result<Self, Error> {
-        let client = Client::connect("host=localhost user=postgres dbname=wfrp", NoTls)?;
+        let host = env::var("DB_HOST").expect("DB_HOST não definido");
+        let user = env::var("DB_USER").expect("DB_USER não definido");
+        let dbname = env::var("DB_DBNAME").expect("DB_DBNAME não definido");
+
+        let connection_string = format!("host={} user={} dbname={}", host, user, dbname);
+
+        let client = Client::connect(&connection_string, NoTls)?;
         Ok(Db { client })
     }
 
